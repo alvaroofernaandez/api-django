52ac68b36f8cdd3b1eb342eeceaa12c7792362e8
Álvaro Fernández Amaro <alvaroofernaandez@gmail.com>
2024-10-20 21:07:59 +0200

    gitignore creado para mejorar la seguridad, y conexiones hechas

--- api_noticias/api_noticias/settings.py
+++ api_noticias/api_noticias/settings.py
@@ -79,14 +79,11 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'noticias_api',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'admin',
         'HOST': 'localhost',
         'PORT': '3306',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-            'ssl': {
-                'disabled': True,
-            }
         }
     }
 }
